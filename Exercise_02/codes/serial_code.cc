88e38ffabadddb2214e7a6370cf1053b7eabd2e8
egonnelli <egonnelli@gmail.com>
2019-07-11 03:03:23 +0200

    Update serial_code.cc

--- Exercise_02/codes/serial_code.cc
+++ Exercise_02/codes/serial_code.cc
@@ -1,32 +1,35 @@
 #include <iostream >
 
-void print_usage( int * a, int N, int nthreads );
+void print_usage(int* a, int N, int nthreads);
 
-int main(int argc, char const *argv[])
-{   
+int main(int argc, char const* argv[])
+{
     const int N = 110;
     int a[N];
     int thread_id = 0;
     int nthreads = 1;
 
-for(int i = 0; i < N; ++i){
-    a[i] = thread_id;
-}
+    for (int i = 0; i < N; ++i) {
+        a[i] = thread_id;
+    }
     print_usage(a, N, nthreads);
-    
+
     return 0;
 }
 
-void print_usage( int * a, int N, int nthreads ) {
-    
+void print_usage(int* a, int N, int nthreads)
+{
+
     int tid, i;
-    
-    for( tid = 0; tid < nthreads; ++tid ) {
+
+    for (tid = 0; tid < nthreads; ++tid) {
         std::cout << tid << ": ";
-            for( i = 0; i < N; ++i ) {
-                if( a[ i ] == tid) std::cout << "*";
-                    else std::cout << " ";
-                                    }
-         std::cout << std::endl;
-}
+        for (i = 0; i < N; ++i) {
+            if (a[i] == tid)
+                std::cout << "*";
+            else
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
 }
