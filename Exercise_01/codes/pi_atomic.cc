fc1dc78e7f8f59d23bbacb0f8edeff911a0cabef
egonnelli <egonnelli@gmail.com>
2019-07-11 02:41:13 +0200

    Update pi_atomic.cc

--- Exercise_01/codes/pi_atomic.cc
+++ Exercise_01/codes/pi_atomic.cc
@@ -5,7 +5,7 @@
 
 //ATOMIC
 
-double function(double x);
+        double function(double x);
 
 int main() {
 
@@ -16,11 +16,11 @@ int main() {
         double h = (b - a) / n;
 
 auto start = std::chrono::high_resolution_clock::now();
-double tstart = omp_get_wtime();
+        double tstart = omp_get_wtime();
 
 #pragma omp parallel
 {
-double local = 0.0;
+        double local = 0.0;
 #pragma omp for
         for (int i = 0; i <= (int) n-1; ++i) {
                 double x_i = h*(i+0.5);
@@ -30,17 +30,17 @@ double local = 0.0;
 integral += local;
 }
 
-double duration = omp_get_wtime() - tstart;
+        double duration = omp_get_wtime() - tstart;
 auto finish = std::chrono::high_resolution_clock::now();
 std::chrono::duration<double> elapsed_vector = finish - start;
 
         double final= 4*h*integral;
-        std::cout << "Number of threads: " << omp_get_max_threads() << "\n";
-        std::cout << "Integral is equal to: " << final << "\n";
-        std::cout << "Time for loop: " << duration << " s\n";
-        std::cout << "Chrono (std::vector) time: " << elapsed_vector.count() << " s\n";
-              return 0;
-    }
-    double function(double x) {
-      return 1.0/( 1.0 + x * x);
-    }
+
+                std::cout << "Number of threads: " << omp_get_max_threads() << "\n";
+                std::cout << "Integral is equal to: " << final << "\n";
+                std::cout << "Time for loop: " << duration << " s\n";
+                std::cout << "Chrono (std::vector) time: " << elapsed_vector.count() << " s\n";
+return 0;}
+
+        double function(double x) {
+                return 1.0/( 1.0 + x * x);}
